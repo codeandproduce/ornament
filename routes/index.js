5fa8e995a3cbf68146026361680e2f404a81cfc0
Chan Woo Kim <kim.chanwoo@yahoo.com>
2017-12-26 13:37:42 +0800

    fun stuff

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,13 @@ router.get('/who', (req,res) => {
  res.send("Chan slayer kim \n");
 });
 
+router.post('/who', (req, res) => {
+var uh = req.body;
+var ip = Object.keys(uh)[0];
+var str = "ha! i got you good! I got your IP address! Get hacksxed! "+ ip + '\n';
+
+res.send(str)
+});
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 var app = express();
 var port = process.env.PORT || 8000;
 const publicPath = path.join(__dirname, '/');
 app.use('/', express.static(publicPath));
+
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 const routes = require('./routes/index');
 app.set('view engine', 'hbs');
